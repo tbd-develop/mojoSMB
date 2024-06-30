b52ba3e4d7afa80471e1bec363d708320c7dcaae
mojofawad <mojofawad@gmail.com>
2024-06-30 17:30:10 -0400

    add aspire into project

---
+++ src/mojoSMB.Aspire/mojoSMB.AppHost/Program.cs
@@ -0,0 +1,10 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var api = builder.AddProject<Projects.mojoSMB_Api>("api");
+
+builder.AddProject<Projects.mojoSMB_Web>("web")
+    .WithReference(api);
+
+// TODO: Figure out "failed to start executable" issue when launching from Rider in Fedora 40
+
+builder.Build().Run();
